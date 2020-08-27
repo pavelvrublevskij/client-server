2b4412c37eeabd03e7361472bb7cd3cb851124d8
Pavel Vrublevskij <pavelvrublevskij@gmail.com>
2020-08-27 13:51:58 +0300

    add liquibase entities for spring-security (eliminate in memory usage in spring security)

--- pvexample/pvexample-api/settings.gradle.kts
+++ pvexample/pvexample-api/settings.gradle.kts
@@ -1,7 +1,9 @@
 rootProject.name = "pvexample-api"
 
 include("security")
+include("security:security-core")
 include("security:security-basic")
+include("security:security-jpa")
 include("pvexample-main")
 include("infra-web")
 include("infra-jpa")
